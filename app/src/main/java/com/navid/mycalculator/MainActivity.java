c085d6d7869d2eaa7c7e7c6c0eab4f2c17693191
Navid Rahman <navidrahman92@gmail.com>
2022-06-24 09:32:19 +0600

    Initial commit

---
+++ app/src/main/java/com/navid/mycalculator/MainActivity.java
@@ -0,0 +1,14 @@
+package com.navid.mycalculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
