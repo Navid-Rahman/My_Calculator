aa7260fc5a9993c8b903b29bc0e648e8baa2043f
Navid Rahman <navidrahman92@gmail.com>
2022-06-24 21:04:33 +0600

    OnClick function added for the number buttons.

--- app/src/main/java/com/navid/mycalculator/MainActivity.java
+++ app/src/main/java/com/navid/mycalculator/MainActivity.java
@@ -10,56 +10,57 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    private EditText result;
-    private EditText newNumber;
-    private TextView displayOperation;
 
-    private Double operand1 = null;
-    private Double operand2 = null;
-    private String pendingOperation = "=";
+    TextView result;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        result = (EditText) findViewById(R.id.result);
-//        newNumber = (EditText) findViewById(R.id.newNumber);
-        displayOperation = (TextView) findViewById(R.id.operation);
+        result = (TextView) findViewById(R.id.textView);
 
-        Button button0 = (Button) findViewById(R.id.buttonZero);
-        Button button1 = (Button) findViewById(R.id.buttonOne);
-        Button button2 = (Button) findViewById(R.id.buttonTwo);
-        Button button3 = (Button) findViewById(R.id.buttonThree);
-        Button button4 = (Button) findViewById(R.id.buttonFour);
-        Button button5 = (Button) findViewById(R.id.buttonFive);
-        Button button6 = (Button) findViewById(R.id.buttonSix);
-        Button button7 = (Button) findViewById(R.id.buttonSeven);
-        Button button8 = (Button) findViewById(R.id.buttonEight);
-        Button button9 = (Button) findViewById(R.id.buttonNine);
-        Button buttonDot = (Button) findViewById(R.id.buttonDot);
-        Button buttonEquals = (Button) findViewById(R.id.buttonEquals);
-        Button buttonDivide = (Button) findViewById(R.id.buttonDivide);
-        Button buttonMultiply = (Button) findViewById(R.id.buttonMultiply);
-        Button buttonMinus = (Button) findViewById(R.id.buttonMinus);
-        Button buttonPlus = (Button) findViewById(R.id.buttonPlus);
-        View.OnClickListener listener = new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Button b = (Button) view;
-                newNumber.append(b.getText().toString());
-            }
-        };
-        button0.setOnClickListener(listener);
-        button1.setOnClickListener(listener);
-        button2.setOnClickListener(listener);
-        button3.setOnClickListener(listener);
-        button4.setOnClickListener(listener);
-        button5.setOnClickListener(listener);
-        button6.setOnClickListener(listener);
-        button7.setOnClickListener(listener);
-        button8.setOnClickListener(listener);
-        button9.setOnClickListener(listener);
-        buttonDot.setOnClickListener(listener);
+    }
+
+    public void numZero(View view){
+        String s = result.getText().toString();
+        result.setText(s+"0");
+    }
+    public void numOne(View view){
+        String s = result.getText().toString();
+        result.setText(s+"1");
+    }
+    public void numTwo(View view){
+        String s = result.getText().toString();
+        result.setText(s+"2");
+    }
+    public void numThree(View view){
+        String s = result.getText().toString();
+        result.setText(s+"3");
+    }
+    public void numFour(View view){
+        String s = result.getText().toString();
+        result.setText(s+"4");
+    }
+    public void numFive(View view){
+        String s = result.getText().toString();
+        result.setText(s+"5");
+    }
+    public void numSix(View view){
+        String s = result.getText().toString();
+        result.setText(s+"6");
+    }
+    public void numSeven(View view){
+        String s = result.getText().toString();
+        result.setText(s+"7");
+    }
+    public void numEight(View view){
+        String s = result.getText().toString();
+        result.setText(s+"8");
+    }
+    public void numNine(View view){
+        String s = result.getText().toString();
+        result.setText(s+"9");
     }
 }
